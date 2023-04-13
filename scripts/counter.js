a7bc41940118854d6aaaf94edb7c41dcce2d782a
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2023-04-13 13:07:31 +0200

    little adjustments

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,38 +1,38 @@
-// Request permission to access the device's accelerometer data
-if (typeof DeviceMotionEvent.requestPermission === 'function') {
-    DeviceMotionEvent.requestPermission().then(permissionState => {
-      if (permissionState === 'granted') {
-        window.addEventListener('devicemotion', deviceMotionHandler);
-      }
-    }).catch(console.error);
-  } else {
-    window.addEventListener('devicemotion', deviceMotionHandler);
-  }
+// // Request permission to access the device's accelerometer data
+// if (typeof DeviceMotionEvent.requestPermission === 'function') {
+//     DeviceMotionEvent.requestPermission().then(permissionState => {
+//       if (permissionState === 'granted') {
+//         window.addEventListener('devicemotion', deviceMotionHandler);
+//       }
+//     }).catch(console.error);
+//   } else {
+//     window.addEventListener('devicemotion', deviceMotionHandler);
+//   }
   
-  // Listen to the device's accelerometer data
-  let accelerationX = 0;
-  let accelerationY = 0;
-  let accelerationZ = 0;
+//   // Listen to the device's accelerometer data
+//   let accelerationX = 0;
+//   let accelerationY = 0;
+//   let accelerationZ = 0;
   
-  function deviceMotionHandler(event) {
-    accelerationX = event.accelerationIncludingGravity.x;
-    accelerationY = event.accelerationIncludingGravity.y;
-    accelerationZ = event.accelerationIncludingGravity.z;
+//   function deviceMotionHandler(event) {
+//     accelerationX = event.accelerationIncludingGravity.x;
+//     accelerationY = event.accelerationIncludingGravity.y;
+//     accelerationZ = event.accelerationIncludingGravity.z;
   
-    // Process the accelerometer data
-    const motion = Math.sqrt(
-      accelerationX * accelerationX +
-      accelerationY * accelerationY +
-      accelerationZ * accelerationZ
-    );
+//     // Process the accelerometer data
+//     const motion = Math.sqrt(
+//       accelerationX * accelerationX +
+//       accelerationY * accelerationY +
+//       accelerationZ * accelerationZ
+//     );
     
-    const threshold = 1; // Adjust this value to fit your walking style
-    let stepCount = 0;
+//     const threshold = 1; // Adjust this value to fit your walking style
+//     let stepCount = 0;
     
-    if (motion > threshold) {
-      stepCount++;
-    }
+//     if (motion > threshold) {
+//       stepCount++;
+//     }
     
-    // Display the step count
-    document.getElementById('step-count').innerHTML = stepCount;
-  }
+//     // Display the step count
+//     document.getElementById('step-count').innerHTML = stepCount;
+//   }
